f4d9aec8273ab458bfb6f4133a758a0ebf31b3ba
ycd633 <ycd633@nineonesoft.com>
2024-04-17 15:14:21 +0900

    'init'

---
+++ nextjs-dashboard/app/dashboard/page.tsx
@@ -0,0 +1,6 @@
+import { fetchRevenue, fetchLatestInvoices } from '@/app/lib/data';
+ 
+
+export default function Page() {
+    return <p>Dashboard Page</p>;
+  }
---
+++ nextjs-dashboard/app/dashboard/test/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return <p>test Page</p>;
+  } 
+
+  
